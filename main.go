54e5f6c25acd1413b4388aa9d8b92cd512da67ac
ReaperMaga <reapermaga@gmail.com>
2024-03-10 23:59:03 +0100

    :fire: Init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./public/views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Static("css/", "public/css")
+
+	users := []User{{Name: "Maga"}, {Name: "Justin"}}
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "This is a title",
+			"Users": users,
+		}, "layouts/default")
+	})
+
+	app.Get("/clicked", func(c *fiber.Ctx) error {
+		return c.Render("partials/test", fiber.Map{
+			"Text": "Works",
+		})
+	})
+	fmt.Println(app.Listen(":3000"))
+}
+
+type User struct {
+	Name string
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/views/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
