d80ca5c5451972eecc3eb8e2728b4499ead0812d
ReaperMaga <reapermaga@gmail.com>
2024-03-11 07:28:39 +0100

    Added environment loading

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
+	"github.com/joho/godotenv"
+	"os"
 	"strconv"
 	"virtual-windows-manager/auth"
 	"virtual-windows-manager/database"
 )
 
 func main() {
-	err := database.Connect("mongodb://localhost:27017", "vwm")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Please provide a env file")
+	}
+
+	err = database.Connect(os.Getenv("MONGODB_CONNECTION_URI"), "vwm")
 	if err != nil {
 		panic("Cannot connect to the database")
 	}
@@ -21,6 +28,7 @@ func main() {
 		Views: engine,
 	})
 	app.Static("css/", "public/css")
+	app.Static("js/", "public/js")
 	app.Static("images/", "public/images")
 
 	users := []User{{Name: "Maga"}, {Name: "Justin"}}
