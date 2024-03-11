e686f8889e973bd4c0391a46a394276454c92947
ReaperMaga <reapermaga@gmail.com>
2024-03-11 06:09:29 +0100

    Added create button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/views/**/*.{html,js}"],
+  content: [
+      "./public/views/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
